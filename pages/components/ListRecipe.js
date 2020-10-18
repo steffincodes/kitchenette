321b7f1869e38a695b12250de250abcc6cf62f1f
Steffin <steffin.codes@gmail.com>
2020-10-18 20:10:20 +0530

    🗃 Getting data from external api 🔨 Along with fallback

--- pages/components/ListRecipe.js
+++ pages/components/ListRecipe.js
@@ -1,15 +1,38 @@
 import getIconImage from "./Utilities";
-export default function ListRecipe() {
+import { ERROR } from "../../lib/global";
+export default function ListRecipe({ props }) {
+  const listOfRecipes = props;
   const recipeList = [];
-  for (let recipe of listOfRecipes) {
+  if (listOfRecipes) {
+    for (const [index, recipe] of listOfRecipes.entries()) {
+      recipeList.push(
+        <div
+          className="flex flex-col justify-center items-center max-w-sm mx-auto"
+          key={index}
+        >
+          <div
+            className={`${
+              recipe.meat ? "bg-red-100" : "bg-green-100"
+            }  shadow-lg rounded-lg overflow-hidden m-3 p-10`}
+          >
+            <div className="text-2xl font-bold py-2">{recipe.name}</div>
+            <ol className="list-decimal pb-0 m-4">
+              {getOverviewList(recipe.overview)}
+            </ol>
+            <div className="text-justify">{getListOfIconHtml(recipe.icon)}</div>
+          </div>
+        </div>
+      );
+    }
+  } else {
     recipeList.push(
-      <div className="flex flex-col justify-center items-center max-w-sm mx-auto">
-        <div className={`${recipe.meat ? "bg-red-200":"bg-green-200"}  shadow-lg rounded-lg overflow-hidden m-3 p-10`}>
-          <div className="text-2xl font-bold py-2">{recipe.name}</div>
-          <ol className="list-decimal pb-0 m-4">
-            {getOverviewList(recipe.overview)}
-          </ol>
-          <div className="text-justify">{getListOfIconHtml(recipe.icon)}</div>
+      <div
+        className="flex flex-col justify-center items-center max-w-sm mx-auto"
+        key="0"
+      >
+        <div className={"bg-white shadow-lg rounded-lg m-3 p-10 text-center"}>
+          <div className="text-2xl font-bold py-2">{ERROR.title}</div>
+          {ERROR.text}
         </div>
       </div>
     );
@@ -22,8 +45,9 @@ export default function ListRecipe() {
             Quick Recipe List
           </h1>
           <p className="lg:w-1/2 w-full leading-relaxed text-base text-gray-500">
-            Just a bunch of filler recipies to get the layout and meta for DB proper. Please do
-            not follow these recipies untill further notice. Be warned! Thanks.
+            Just a bunch of filler recipies to get the layout and meta for DB
+            proper. Please do not follow these recipies untill further notice.
+            Be warned! Thanks.
           </p>
         </div>
         <div className="flex flex-wrap m-3">{recipeList}</div>
@@ -33,16 +57,16 @@ export default function ListRecipe() {
 }
 function getOverviewList(listOfOverview) {
   const overviewList = [];
-  for (let overview of listOfOverview) {
-    overviewList.push(<li>{overview}</li>);
+  for (let [index, overview] of listOfOverview.entries()) {
+    overviewList.push(<li key={index}>{overview}</li>);
   }
   return overviewList;
 }
 function getListOfIconHtml(listOfIcons) {
   const iconList = [];
-  for (let icon of listOfIcons) {
+  for (let [index, icon] of listOfIcons.entries()) {
     iconList.push(
-      <button className="p-1 pb-2 text-sm cursor-not-allowed">
+      <button className="p-1 pb-2 text-sm cursor-not-allowed" key={index}>
         <img
           className="h-8 w-8 object-cover mx-1"
           src={getIconImage(icon)}
@@ -53,111 +77,3 @@ function getListOfIconHtml(listOfIcons) {
   }
   return iconList;
 }
-
-const listOfRecipes = [
-  {
-    name: "Garlic Chutney",
-    time: "30m",
-    meat: false,
-    icon: ["garlic", "onion", "chili"],
-    overview: [
-      "Soak tamarind and extract water",
-      "Sautee Mustard seeds, Curry Leaves, Garlic, Onion and Chili in Oil",
-      "Cool and grind with coconut",
-    ],
-  },
-  {
-    name: "Tomato Chutney",
-    time: "30m",
-    meat: false,
-    icon: ["tomato", "onion", "chili"],
-    overview: [
-      "Sautee Mustard seeds, Curry Leaves, Tomato, Onion and Chili in Oil",
-      "Cool and grind with coconut",
-    ],
-  },
-  {
-    name: "Fried Rice",
-    time: "30m",
-    meat: false,
-    icon: ["rice", "vegetables"],
-    overview: [
-      "Soak rice and drain",
-      "Boil rice with salt and oil and drain",
-      "Chop available vegetables and saute in oil and ginger",
-      "Add Sauce, pepper and rice and stir",
-    ],
-  },
-  {
-    name: "Schezwan Fried Rice",
-    time: "30m",
-    meat: false,
-    icon: ["rice", "sauce", "vegetables"],
-    overview: [
-      "Soak rice and drain",
-      "Boil rice with salt and oil and drain",
-      "Chop available vegetables and saute in oil and ginger",
-      "Add Sauce, pepper and rice and stir",
-    ],
-  },
-  {
-    name: "Fried Noodles",
-    time: "30m",
-    meat: false,
-    icon: ["noodles", "vegetables"],
-    overview: [
-      "Boil water with salt and oil",
-      "Add noodles into hot water",
-      "Strain and rince in cold water",
-      "Add oil and toss in fried vegetables",
-    ],
-  },
-  {
-    name: "Schezwan Fried Noodles",
-    time: "30m",
-    meat: false,
-    icon: ["noodles", "sauce", "vegetables"],
-    overview: [
-      "Boil water with salt and oil",
-      "Add noodles into hot water",
-      "Strain and rince in cold water",
-      "Add oil, sauce, vinegar and toss in fried vegetables",
-    ],
-  },
-  {
-    name: "Mutton Biriyani",
-    time: "60m",
-    meat: true,
-    icon: ["rice", "meat"],
-    overview: [
-      "Marinate meat",
-      "Sautee biriyani masala and onion and add meat",
-      "Roast rice in ghee, cook and drain",
-      "Add Rice to masala and low heat",
-    ],
-  },
-  {
-    name: "Chicken Biriyani",
-    time: "50m",
-    meat: true,
-    icon: ["rice", "chicken"],
-    overview: [
-      "Marinate meat",
-      "Sautee biriyani masala and onion and add meat",
-      "Roast rice in ghee, cook and drain",
-      "Add Rice to masala and low heat",
-    ],
-  },
-  {
-    name: "Prawn Biriyani",
-    time: "50m",
-    meat: true,
-    icon: ["rice", "prawn"],
-    overview: [
-      "Marinate meat",
-      "Sautee biriyani masala and onion and add meat",
-      "Roast rice in ghee, cook and drain",
-      "Add Rice to masala and low heat",
-    ],
-  },
-];
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,22 @@
 import ListRecipe from "./components/ListRecipe";
 import Meta from "./components/Meta";
 import FloatingButton from "./components/FloatingButton";
-export default function Home() {
+export default function Home(props) {
   return (
     <div>
       <Meta />
       <FloatingButton />
-      <ListRecipe />
+      <ListRecipe props={props.recipies} />
     </div>
   );
+}
+
+export async function getStaticProps(context) {
+  let recipies = [];
+  let res = '';
+  res = await fetch(process.env.JSON_BLOB_URL+process.env.JSON_BLOB_ID);
+  if (res){
+    recipies = await res.json();
+  }
+  return { props: { recipies } };
 }
