5762e5580333ae208768daa2d0ec2ce8e0232b93
Steffin <steffin.codes@gmail.com>
2020-10-18 19:48:42 +0530

    🍻 Changed class to className and styled buttons

--- pages/components/FloatingButton.js
+++ pages/components/FloatingButton.js
@@ -2,26 +2,26 @@ export default function FloatingButton() {
   return (
     <div>
       <a
-        class="fixed rounded-full h-16 w-16 text-center z-50 bottom-right bg-fab pt-3"
+        className="fixed rounded-full h-16 w-16 text-center z-50 bottom-right bg-fab pt-3"
         id="fab-menu"
       >
-        <span class="mt-4 fab-icon">🍔</span>
+        <span className="mt-4 fab-icon">🍔</span>
       </a>
-      <ul class="fixed bottom-right-2 list-none" id="fab">
-        <li class="list-none my-2">
+      <ul className="fixed bottom-right-2 list-none" id="fab">
+        <li className="list-none my-2">
           <a
             id="fab-menu"
-            class="block rounded-full bg-fab h-16 w-16 text-center pt-3"
+            className="block rounded-full bg-fab h-16 w-16 text-center pt-3"
           >
-            <span class="mt-4 fab-icon">🆕</span>
+            <span className="mt-4 fab-icon">🆕</span>
           </a>
         </li>
-        <li class="list-none my-2">
+        <li className="list-none my-2">
           <a
             id="fab-menu"
-            class="block rounded-full bg-fab h-16 w-16 text-center pt-3"
+            className="block rounded-full bg-fab h-16 w-16 text-center pt-3"
           >
-            <span class="mt-4 fab-icon">⏲️</span>
+            <span className="mt-4 fab-icon">⏲️</span>
           </a>
         </li>
       </ul>
